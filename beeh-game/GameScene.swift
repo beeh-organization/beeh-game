2b89c92cbb74ca8f6534c9598d323c57fb8ac679
thiagoHBA <thiagohhab@gmail.com>
2023-03-30 10:37:53 -0300

    first commit

---
+++ beeh-game/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  beeh-game
+//
+//  Created by Thiago Henrique on 30/03/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        self.backgroundColor = .green
+    }
+}
