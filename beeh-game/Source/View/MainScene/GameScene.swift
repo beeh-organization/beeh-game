962465dae750a78df8cdf625f8fbb2193c66d9ec
thiagoHBA <thiagohhab@gmail.com>
2023-04-04 10:06:43 -0300

    add sheep to the map

--- beeh-game/Source/View/MainScene/GameScene.swift
+++ beeh-game/Source/View/MainScene/GameScene.swift
@@ -10,6 +10,10 @@ import GameplayKit
 
 class GameScene: SKScene {
     private let background: SKSpriteNode = SKSpriteNode(imageNamed: "background1")
+    private let sheep: SKSpriteNode = {
+        let atlas = SKTextureAtlas(named: "SheepWalk")
+        return SKSpriteNode(texture: atlas.textureNamed("sheep_walk1"))
+    }()
 
     override func didMove(to view: SKView) {
         buildLayout()
@@ -18,12 +22,18 @@ class GameScene: SKScene {
 
 extension GameScene: ViewCoding {
     func setupConstraints() {
+        guard let view = view else { return }
 //        background.position = CGPoint(x: size.width/2, y: size.height/2) //TODO - Mapa infinito
         background.anchorPoint = CGPoint.zero
         background.zPosition = -1
+        
+        sheep.position = CGPoint(x: view.frame.midX, y:  view.frame.midY + 200)
+        sheep.size.width *= 0.25
+        sheep.size.height *= 0.25
     }
     
     func addViewHierarchy() {
         addChild(background)
+        addChild(sheep)
     }
 }
